ada3d4c9d5e1d3c02a3cbe7c057ce094716b53ac
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2019-07-28 01:51:09 +0530

    Add an ftyp box type and its parser

--- mp4box/box.py
+++ mp4box/box.py
@@ -1,12 +1,19 @@
 class Box:
-    def __init__(self, size: int, box_type: int, uuid):
+    def __init__(self, size: int, box_type: str, uuid):
         self.size = size
         self.type = box_type
         self.uuid = uuid
 
 class FullBox(Box):
-    def __init__(self, size: int, box_type: int, uuid, v: int, f: int):
+    def __init__(self, size: int, box_type: str, uuid, v: int, f: int):
         super().__init__(size, box_type, uuid)
         #Boxes with an unrecognized version shall be ignored and skipped. 
         self.version = v
         self.flags = f
+
+class FileTypeBox(Box):
+    def __init__(self, size: int, major_brand: int, minor_version: int, compatible_brands: [int]):
+        super().__init__(size, 'ftyp')
+        self.major_brand = major_brand
+        self.minor_brand = minor_version
+        self.compatible_brands = compatible_brands
--- mp4box/box_parser.py
+++ mp4box/box_parser.py
@@ -1,8 +1,19 @@
-from io import BufferedReader
-
 class BoxParser:
-    def __init__(self, stream):
-        self.reader = open(stream, "rb")
+    def __init__(self, file):
+        self.reader = StreamReader(file)
 
     def parse(self):
-        pass
+        size = self.reader.read32()
+        type = self.reader.read32_as_str()
+        if type is 'ftyp':
+            self.parse_ftyp(size)
+
+    def parse_ftyp(self, size):
+        major_brand = self.reader.read32_as_str()
+        minor_version = self.reader.read32()
+        compatible_brands = []
+        cnt = size - 16
+        while cnt < size:
+            compatible_brands.append(self.reader.read32_as_str())
+            cnt += 4
+        box = FileTypeBox(size, 'ftyp', major_brand, minor_version, compatible_brands)
