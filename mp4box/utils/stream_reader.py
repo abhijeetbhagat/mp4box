336ed29e427b33de77f2b74bad9f3a455c1675e6
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2019-07-29 23:07:03 +0530

    StreamReader should work with paths or streams

--- mp4box/utils/stream_reader.py
+++ mp4box/utils/stream_reader.py
@@ -2,16 +2,24 @@ from io import BufferedReader
 
 class StreamReader:
     def __init__(self, file):
-        self.stream = open(file, "rb")
+        if isinstance(file, str):
+            self.stream = open(file, "rb")
+        else:
+            self.stream = file
 
-    def __del__(self):
-        self.stream.close()
+    #def __del__(self):
+    #    self.stream.close()
 
     def read8(self):
-        return stream.read(1)
+        return self.stream.read(1)
 
     def read32(self):
-        return int.from_bytes(stream.read(4)[0:], "big")
+        return int.from_bytes(self.stream.read(4), "big")
 
     def read32_as_str(self):
-        return stream.read(4)[0:].decode('utf-8')
+        return self.stream.read(4)[0:].decode('utf-8')
+
+    def reached_eof(self):
+        #TODO abhi: should calculate size of the file in the ctor and
+        #check if tell() has crossed it
+        return self.stream.peek(1) == b''
