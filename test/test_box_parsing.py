b125c50102253ef72344b9e06e90ddfaaf74b6fc
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2019-08-12 21:47:04 +0530

    Add stss parsing test

--- test/test_box_parsing.py
+++ test/test_box_parsing.py
@@ -3,6 +3,7 @@ from tempfile import TemporaryFile
 from mp4box.utils.stream_reader import StreamReader
 from mp4box.parsing.ftyp import *
 from mp4box.parsing.stts import *
+from mp4box.parsing.stss import *
 
 class TestBoxParsingIsolation(unittest.TestCase):
     def test_ftyp(self):
@@ -33,6 +34,21 @@ class TestSampleTableParsing(unittest.TestCase):
            stts = parse_stts(reader, size)
            self.assertNotEqual(stts, None)
            self.assertEqual(stts.entry_count, 1)
+    
+    def test_stss(self):
+        with TemporaryFile() as f:
+            f.write(b'\x00\x00\x00\x4c\x73\x74\x73\x73\x00\x00\x00\x00\x00'
+                    b'\x00\x00\x0f\x00\x00\x00\x01\x00\x00\x00\x31\x00\x00'
+                    b'\x00\x61\x00\x00\x00\x91\x00\x00\x00\xc1\x00\x00\x00'
+                    b'\xf1\x00\x00\x01\x21\x00\x00\x01\x51\x00\x00\x01\x81'
+                    b'\x00\x00\x01\xb1\x00\x00\x01\xe1\x00\x00\x02\x11\x00\x00\x02\x41'
+                    b'\x00\x00\x02\x71\x00\x00\x02\xa1')
+            f.seek(0)
+            reader = StreamReader(f)
+            size = reader.read32()
+            _ = reader.read32()
+            stss = parse_stss(reader, size)
+            self.assertNotEqual(stss, None)
 
 if __name__ == '__main__':
     test_classes_to_run = [TestBoxParsingIsolation, TestSampleTableParsing]
