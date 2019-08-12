cf98f7e259677eeef02c0656c0d48bcb0fbf490b
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2019-08-12 21:46:35 +0530

    Fix stts parsing - read version too

--- mp4box/parsing/stts.py
+++ mp4box/parsing/stts.py
@@ -1,10 +1,10 @@
 from mp4box.box import TimeToSampleBox
 
 def parse_stts(reader, size):
-    entry_count = reader.read32()
-    box = TimeToSampleBox(size, 0, 0)
-    box.entry_count = entry_count
-    for _ in range(0, entry_count):
+    version = reader.read32()
+    box = TimeToSampleBox(size, version, 0)
+    box.entry_count = reader.read32()
+    for _ in range(0, box.entry_count):
         box.sample_count.append(reader.read32())
         box.sample_delta.append(reader.read32())
     return box
