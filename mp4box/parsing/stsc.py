c20af518ca04c62d0d6f83f81e278bee3ccf5fb5
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2019-08-12 23:01:14 +0530

    Fix stsc parsing - read version too

--- mp4box/parsing/stsc.py
+++ mp4box/parsing/stsc.py
@@ -1,10 +1,10 @@
 from mp4box.box import SampleToChunkBox
 
 def parse_stsc(reader, size):
-    entry_count = reader.read32()
-    box = SampleToChunkBox(size, 0, 0)
-    box.entry_count = entry_count
-    for _ in range(0, entry_count):
+    version = reader.read32()
+    box = SampleToChunkBox(size, version, 0)
+    box.entry_count = reader.read32()
+    for _ in range(0, box.entry_count):
         box.first_chunk.append(reader.read32())
         box.samples_per_chunk.append(reader.read32())
         box.sample_description_index.append(reader.read32())
