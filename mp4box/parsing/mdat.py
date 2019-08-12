97b04e2a3ef78ca1cef6a6a19a4960b1de5ea746
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2019-08-12 17:40:15 +0530

    Add an mdat box

---
+++ mp4box/parsing/mdat.py
@@ -0,0 +1,8 @@
+from mp4box.box import MediaDataBox
+
+def parse_mdat(reader, my_size):
+    box = MediaDataBox(my_size, self.reader.current_pos())
+    #we have nothing to do with media data as of now
+    #and advancing the file ptr is necessary
+    reader.skip(my_size)
+    return box
