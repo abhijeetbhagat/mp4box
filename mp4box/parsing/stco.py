bbd1828f6d69f7b6bfc38d3bda98b8ca65f0339f
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2019-08-13 21:00:02 +0530

    Fix stco parsing and add a test

--- mp4box/parsing/stco.py
+++ mp4box/parsing/stco.py
@@ -1,7 +1,8 @@
 from mp4box.box import ChunkOffsetBox
 
 def parse_stco(reader, size):
-    box = ChunkOffsetBox(size, 0, 0)
+    version = reader.read32()
+    box = ChunkOffsetBox(size, version, 0)
     box.entry_count = reader.read32()
     for _ in range(0, box.entry_count):
         box.chunk_offset.append(reader.read32())
