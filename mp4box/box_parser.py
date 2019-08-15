f0a3fd5cf696bb3d821d7c3c918cf2d9348ecbed
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2019-08-15 13:12:20 +0530

    Check if parsing is already done before beginning

--- mp4box/box_parser.py
+++ mp4box/box_parser.py
@@ -9,35 +9,39 @@ from mp4box.utils.exceptions import InvalidBoxError
 class BoxParser:
     def __init__(self, file):
         self.reader = StreamReader(file)
-        self.root = RootBox()
+        self.root = None
 
     def parse(self):
-        size = self.reader.read32()
-        type = self.reader.read32_as_str()
-        while not self.reader.reached_eof():
-            if type == 'ftyp':
-                self.root.ftyp = parse_ftyp(self.reader, size)
-            elif type == 'moov':
-                self.root.moov = parse_moov(self.reader, size)
-            elif type == 'free':
-                self.root.free = parse_free(self.reader, size)
-            elif type == 'mdat':
-                self.root.mdats.append(parse_mdat(self.reader, size))
-            else:
-                raise InvalidBoxError("type %s unknown" % type, None)
-            if self.reader.reached_eof():
-                break
-            #At the end of current box parsing, the file pointer will be
-            #ready to read the size and type of the next box
+        if not self.root:
+            self.root = RootBox()
             size = self.reader.read32()
             type = self.reader.read32_as_str()
+            while not self.reader.reached_eof():
+                if type == 'ftyp':
+                    self.root.ftyp = parse_ftyp(self.reader, size)
+                elif type == 'moov':
+                    self.root.moov = parse_moov(self.reader, size)
+                elif type == 'free':
+                    self.root.free = parse_free(self.reader, size)
+                elif type == 'mdat':
+                    self.root.mdats.append(parse_mdat(self.reader, size))
+                else:
+                    raise InvalidBoxError("type %s unknown" % type, None)
+                if self.reader.reached_eof():
+                    break
+                #At the end of current box parsing, the file pointer will be
+                #ready to read the size and type of the next box
+                size = self.reader.read32()
+                type = self.reader.read32_as_str()
 
-        #Either box parsing was successful or it has errors
+            #Either box parsing was successful or it has errors
 
     def get_tree(self):
         return self.root
 
     def get_all_info(self):
+        #TODO abhi: not sure if the metadata should be structured as a dict
+        #or something else. For now, just return a dict.
         out = {}
 
         out["duration"] = root.get_duration()
