7050f54796e6b4f576669e06fc050d83d218032f
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2019-08-17 01:09:39 +0530

    Move tests to a different file

--- test/test_box_parsing.py
+++ test/test_box_parsing.py
@@ -10,6 +10,9 @@ from mp4box.parsing.stco import *
 from mp4box.parsing.ctts import *
 from mp4box.parsing.stsz import *
 from mp4box.parsing.tkhd import *
+from mp4box.parsing.mvhd import *
+from mp4box.parsing.mdhd import *
+from mp4box.parsing.hdlr import *
 
 class TestTopLevelBoxes(unittest.TestCase):
     def test_ftyp(self):
@@ -141,8 +144,55 @@ class TestTraks(unittest.TestCase):
                 self.assertEqual(tkhd.width, 320)
                 self.assertEqual(tkhd.height, 240)
 
+class TestMoov(unittest.TestCase):
+     def test_mvhd(self):
+        with TemporaryFile() as f:
+            f.write(b"\x00\x00\x00\x6c\x6d\x76\x68\x64\x00\x00\x00\x00\xc1\x02\x17\x10\xc1"
+                    b"\x02\x17\x63\x00\x00\x02\x58\x00\x00\x4a\xb3\x00\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+                    b"\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+                    b"\x00\x00\x00\x40\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+                    b"\x00\x00\x00\x00\x00\x00\x03")
+            f.seek(0)
+            with StreamReader(f) as reader:
+                size = reader.read32()
+                _ = reader.read32()
+                mvhd = parse_mvhd(reader, size)
+                self.assertIsNot(mvhd, None) 
+                self.assertEqual(mvhd.next_track_id, 3) 
+
+class TestMdia(unittest.TestCase):
+     def test_video_mdhd(self):
+        with TemporaryFile() as f:
+            f.write(b"\x00\x00\x00\x20\x6d\x64\x68\x64\x00\x00\x00"
+                    b"\x00\xc1\x02\x17\x13\xc1\x02\x17\x64\x00\x00\x00\x0f\x00\x00\x01\xd9\x15\xc7\x00\x00")
+            f.seek(0)
+            with StreamReader(f) as reader:
+                size = reader.read32()
+                _ = reader.read32()
+                mdhd = parse_mdhd(reader, size)
+                self.assertNotEqual(mdhd, None)
+                self.assertEqual(mdhd.timescale, 15)
+                self.assertEqual(mdhd.duration, 473)
+                self.assertEqual(mdhd.pad, 0)
+                self.assertEqual(mdhd.language, 'eng')
+
+     def test_video_hdlr(self):
+        with TemporaryFile() as f:
+            f.write(b'\x00\x00\x00\x44\x68\x64\x6c\x72\x00\x00\x00\x00\x00\x00'
+                    b'\x00\x00\x76\x69\x64\x65\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x32\x36\x34'
+                    b'\x40\x47\x50\x41\x43\x30\x2e\x38\x2e\x30\x2d\x72\x65\x76\x39\x2d\x67\x36'
+                    b'\x65\x34\x61\x66\x30\x35\x62\x2d\x6d\x61\x73\x74\x65\x72\x00')
+            f.seek(0)
+            with StreamReader(f) as reader:
+                size = reader.read32()
+                _ = reader.read32()
+                hdlr = parse_hdlr(reader, size)
+                self.assertNotEqual(hdlr, None)
+                self.assertEqual(hdlr.handler_type, 'vide')
+                self.assertEqual(hdlr.name, '264@GPAC0.8.0-rev9-g6e4af05b-master\x00')
+
 if __name__ == '__main__':
-    test_classes_to_run = [TestTopLevelBoxes, TestSampleTables, TestTraks]
+    test_classes_to_run = [TestTopLevelBoxes, TestSampleTables, TestTraks, TestMoov, TestMdia]
 
     loader = unittest.TestLoader()
 
