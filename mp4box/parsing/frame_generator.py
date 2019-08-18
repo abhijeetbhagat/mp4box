0fcd4434a740a123a2782fd666658588364d9099
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2019-08-18 22:48:31 +0530

    Add a FrameGenerator class

---
+++ mp4box/parsing/frame_generator.py
@@ -0,0 +1,8 @@
+class FrameGenerator:
+    def __init__(self, media_type, traks, mdats):
+        self.media_type = media_type
+        self.traks = traks
+        self.mdats = mdats
+
+    def get(self):
+        pass
