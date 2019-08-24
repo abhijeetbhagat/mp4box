27902745311fb89ddabb1453dbc0a82b66c4e2fa
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2019-08-24 10:17:18 +0530

    Split FrameGenerator into AudioFrameGenerator and VideoFrameGenerator

--- mp4box/parsing/frame_generator.py
+++ mp4box/parsing/frame_generator.py
@@ -1,8 +1,14 @@
+#FrameGenerator should work with both stbls and truns
+#It should work with one mdat at a time.
 class FrameGenerator:
-    def __init__(self, media_type, traks, mdats):
-        self.media_type = media_type
-        self.traks = traks
-        self.mdats = mdats
-
     def get(self):
+        #TODO abhi: implemented frame yielding here
+        pass
+
+class AudioFrameGenerator:
+    def __init__(self, trak, mdat):
+        pass
+
+class VideoFrameGenerator:
+    def __init__(self, trak, mdat):
         pass
