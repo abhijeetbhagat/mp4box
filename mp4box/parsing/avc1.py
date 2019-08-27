7940e4560d4da4750b411958b0ebb3793f5937bf
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2019-08-27 22:30:58 +0530

    Add avc1 parsing

---
+++ mp4box/parsing/avc1.py
@@ -0,0 +1,21 @@
+from mp4box.box import AVC1Box
+from mp4box.parsing.btrt import parse_btrt
+
+def parse_avc1(reader, my_size):
+    box = AVC1Box(my_size)
+    cnt = 0
+    while not reader.reached_eof() and cnt < my_size:
+        size = reader.read32()
+        type = reader.read32_as_str()
+        cnt += size
+        if type is 'avcC':
+            box.avcc = parse_avcc(reader, size)
+        elif type is 'btrt':
+            box.btrt = parse_btrt(reader, size)
+        else:
+            raise InvalidBoxError("type % is unknown" % type, None)
+
+    return box
+
+
+
