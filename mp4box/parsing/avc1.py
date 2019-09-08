2249fd3d42c488be0f75526736eb58e647a2ba0b
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2019-09-08 13:53:46 +0530

    Parse avc1 fields

--- mp4box/parsing/avc1.py
+++ mp4box/parsing/avc1.py
@@ -1,10 +1,34 @@
 from mp4box.box import AVC1Box
+from mp4box.parsing.avcc import parse_avcc
 from mp4box.parsing.btrt import parse_btrt
 from mp4box.utils.exceptions import InvalidBoxError
 
 def parse_avc1(reader, my_size):
     box = AVC1Box(my_size)
-    cnt = 0
+    reader.skip(6) #reserved
+    box.data_ref_index = reader.read16()
+    box.vid_enc_version = reader.read16()
+    box.vid_enc_revision_lvl = reader.read16()
+    box.vid_enc_vendor = reader.read32_as_str()
+    box.vid_temporal_quality = reader.read32()
+    box.vid_spatial_quality = reader.read32()
+    box.vid_frame_pixel_size = reader.read32()
+    box.vid_resolution = reader.read64()
+    box.vid_data_size = reader.read32()
+    box.vid_frame_count = reader.read16()
+    box.vid_enc_name_len = reader.read8()
+    box.vid_enc_name = reader.readn_as_str(box.vid_enc_name_len)
+
+    if box.vid_enc_name_len < 31:
+        #if the encoder name is less than 31, then pad with 0s
+        box.vid_enc_name = box.vid_enc_name + '0' * (31 - box.vid_enc_name_len)
+        reader.skip(31 - box.vid_enc_name_len)
+
+    box.vid_pixel_depth = reader.read16()
+    box.vid_color_tbl_id = reader.read16()
+
+    cnt = 86 #avc1 len + name + number of bytes parsed above
+
     while not reader.reached_eof() and cnt < my_size:
         size = reader.read32()
         type = reader.read32_as_str()
@@ -14,7 +38,7 @@ def parse_avc1(reader, my_size):
         elif type == 'btrt':
             box.btrt = parse_btrt(reader, size)
         else:
-            raise InvalidBoxError("type % is unknown" % type, None)
+            raise InvalidBoxError("type %s is unknown" % type, None)
 
     return box
 
